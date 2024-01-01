d9a5aa02684b926f3bf833aa584e6275826fed1e
FrankMutethia <frankmutethia81@gmail.com>
2024-01-01 23:26:17 +0300

    shopping cart components setup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,24 @@
+import { Link, NavLink } from "react-router-dom";
+import Logo from "../assets/logo.png";
+import "./Header.css";
 export const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <Link to= "/" className= "logo">
+        <img src={Logo} alt="Shopmate Logo" />
+        <span>Shopping Cart</span>
+      </Link>
+      <nav className="navigation">
+      <NavLink to="/" className="link">Home</NavLink>
+      <NavLink to="/" className="link">Cart</NavLink>
+      </nav>
+      <Link to="/cart" className="items">
+        <span>
+          Cart: 2
+        </span>
+      </Link>
+    </header>
   )
 }
 
-export default Header
+
